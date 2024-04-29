bba7c7a0e7b65476db3dad78c882a3610f74062f
Himchan Park <chan150@kaist.ac.kr>
2024-04-29 19:00:42 +0900

    🔨 TODO: ble device selector 구현

--- lib/src/widget/ble_device_select.dart
+++ lib/src/widget/ble_device_select.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus_windows/flutter_blue_plus_windows.dart';
 
 class BleDeviceSelect extends StatelessWidget {
   const BleDeviceSelect({super.key});
@@ -6,6 +7,13 @@ class BleDeviceSelect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: stream builder ~ list ~ auto scan 가능한지 테스트
+    return StreamBuilder(
+      stream: FlutterBluePlus.scanResults.expand((e) => e),
+      builder: (context, snapshot) {
+        return Text(snapshot.data.toString());
+      },
+    );
+
     return ListView(
       shrinkWrap: true,
       children: [
