3dc061874b030a41e9c03cbe20c881f4fd8d3d37
Himchan Park <chan150@kaist.ac.kr>
2024-04-29 15:29:45 +0900

    🔨 add manager

--- lib/nrf_ble_dfu.dart
+++ lib/nrf_ble_dfu.dart
@@ -1,7 +1,3 @@
 library nrf_ble_dfu;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:nrf_ble_dfu/nrf_ble_dfu.dart';
---
+++ lib/src/nrf_ble_dfu.dart
@@ -0,0 +1,7 @@
+class NrfBleDfu {
+  factory NrfBleDfu() => _instance;
+
+  static final _instance = NrfBleDfu._internal();
+
+  NrfBleDfu._internal();
+}
